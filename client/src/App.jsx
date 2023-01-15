ed710fd27a15584f6997261d07c498a3ae89fb0d
IgnacioPerezEtc <nachoperezetc@gmail.com>
2023-01-15 19:03:25 -0300

    First commit

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import LandingPage from "./components/LandingPage/LandingPage.jsx";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar.jsx";
+import Form from "./components/Form/Form.jsx";
+import { Provider } from "react-redux";
+import  store  from "./redux/store.js";
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <NavBar></NavBar>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/form" element={<Form />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+export default App;
