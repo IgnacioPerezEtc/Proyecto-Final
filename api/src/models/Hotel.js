7d8f69cb71d59b9cd7652cc886bfcecd26563b99
Nina1823 <lilianarivera310@gmail.com>
2023-01-18 21:51:35 -0500

    Modelos

--- api/src/models/Hotel.js
+++ api/src/models/Hotel.js
@@ -6,59 +6,70 @@ module.exports = (sequelize) => {
       allowNull: false,
       primaryKey: true
     },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate:{
-        len:[3,150]
+      validate: {
+        len: [3, 150]
       }
     },
+
     room: {
       type: DataTypes.INTEGER,
       allowNull: false,
     },
+
     location: {
       type: DataTypes.STRING,
       allowNull: false
     },
+
     description: {
       type: DataTypes.TEXT,
       validate: {
-        len:[10,5000]
+        len: [10, 5000]
       },
       allowNull: false
     },
+
     parking: {
       type: DataTypes.BOOLEAN,
       allowNull: false
     },
+
     pictureHome: {
       type: DataTypes.STRING,
       validate: { isUrl: true }
     },
+
     pictureDetail: {
       type: DataTypes.ARRAY(DataTypes.STRING),
       validate: { isUrl: true }
     },
+
     rating: {
-      type: DataTypes.FLOAT,
+      type: DataTypes.REAL,
       allowNull: false,
-      validate:{
-        min:0,
-        max:5
+      validate: {
+        min: 0,
+        max: 5
       }
     },
+
     languages: {
       type: DataTypes.ARRAY(DataTypes.STRING),
       allowNull: false
     },
+
     category: {
       type: DataTypes.INTEGER,
-      allowNull:false,
-      validate:{
-        min:0,
-        max:5
+      allowNull: false,
+      validate: {
+        min: 0,
+        max: 5
       }
     }
-  });
+  }, { timestamps: false }
+  )
 };
---
+++ api/src/models/Reservation.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('reservation', {
+        people:{
+            type: DataTypes.INTEGER,
+            allowNull:false
+        },
+        startDate: {
+            type: DataTypes.DATEONLY,
+            allowNull: false
+        },
+        endDate:{
+            type: DataTypes.DATEONLY,
+            allowNull:false
+        },
+    })
+}
