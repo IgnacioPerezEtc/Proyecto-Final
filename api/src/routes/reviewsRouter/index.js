b6b17adde3899fdcfeb18be211d1b745f6f3d05d
unknown <hruales1138@gmail.com>
2023-01-19 15:08:00 -0500

    ruta de reviews

--- api/src/routes/reviewsRouter/index.js
+++ api/src/routes/reviewsRouter/index.js
@@ -1,22 +1,22 @@
 const { Router } = require('express');
-const { Review } = require('../../db');
+const { Commentary } = require('../../db');
 
 const router = Router();
 
-// router.post('/', async (req, res) => {
-//     try {
-//         const { idName, idHotel, rating } = req.body;
-//         if(rating) {
-//             const newReview = await Review.create(req.body);
-//             await newReview.addNames(idName);
-//             await newReview.addHotels(idHotel);
-//             res.send(newReview)
-//         }
-//         else res.status(404).send(error.message)
+router.post('/', async (req, res) => {
+    try {
+        const { idUser, idHotel, ratingComment } = req.body;
+        if(ratingComment) {
+            const newCommentary = await Commentary.create(req.body);
+            await newCommentary.addUsers(idUser);
+            await newCommentary.addHotels(idHotel);
+            res.send(newCommentary)
+        }
+        else res.status(404).send(error.message)
         
-//     } catch (error) {
-//         res.status(404).send(error.message)
-//     }
-// })
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
+})
 
 module.exports = router;
