1f6248d7f1e645990f8ebe6314370128130acf06
IgnacioPerezEtc <nachoperezetc@gmail.com>
2023-01-18 21:44:13 -0300

    agregando el footer modularizado

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,11 @@
 import React from "react";
-
+import style from "./LandingPage.module.css";
+import Footer from "../Footer/Footer.jsx"
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div>
+ <Footer />
+    </div>
+  );
 };
 export default LandingPage;
