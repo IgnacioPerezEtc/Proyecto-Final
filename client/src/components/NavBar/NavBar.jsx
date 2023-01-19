52fb91c09a9c6fa729bc8f3c4b5585fca2270fa7
Rodri7Roman <romanrodri03@gmail.com>
2023-01-19 12:12:03 -0300

    agregando header y navbar, jsx y css

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,6 +1,74 @@
 import React from "react";
+import "./NavBar.css";
+import facebook from "../../assets/icons/facebook.svg";
+import instagram from "../../assets/icons/instagram.svg";
+import twitter from "../../assets/icons/twitter.svg";
+import youtube from "../../assets/icons/youtube.svg";
 
-const NavBar = () => {
-  return <div>NavBar</div>;
+const NavBar = (props) => {
+  return (
+    <div className="container-navbar">
+      <nav className="container-nav">
+        <div>
+          <ul className="ul-nav">
+            <li>
+              <a href="" className="link-landing">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="" className="link-landing">
+                Hotels
+              </a>
+            </li>
+            <li>
+              <a href="" className="link-landing">
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="" className="link-landing">
+                About Us
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <ul className="ul-nav-redes">
+            <li>
+              <a href="https:/www.instagram.com" target={"_blank"}>
+                <button>
+                  <img src={instagram} alt="logo instagram" />
+                </button>
+              </a>
+            </li>
+            <li>
+              <a href="https:/www.facebook.com" target={"_blank"}>
+                <button>
+                  <img src={facebook} alt="logo facebook" />
+                </button>
+              </a>
+            </li>
+            <li>
+              <a href="https:/www.youtube.com" target={"_blank"}>
+                <button>
+                  <img src={youtube} alt="logo youtube" />
+                </button>
+              </a>
+            </li>
+            <li>
+              <a href="https:/www.twitter.com" target={"_blank"}>
+                <button>
+                  <img src={twitter} alt="logo twitter" />
+                </button>
+              </a>
+            </li>
+
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 };
 export default NavBar;
