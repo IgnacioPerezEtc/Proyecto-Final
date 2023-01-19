7b3965d6c88b4ac618a747ab16dc6625fd4368e7
Rodri7Roman <romanrodri03@gmail.com>
2023-01-19 13:22:32 -0300

    agregando Header en landing page

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import style from "./LandingPage.module.css";
-import Footer from "../Footer/Footer.jsx"
+import Footer from "../Footer/Footer.jsx";
+import Header from "../Header/Header";
 const LandingPage = () => {
   return (
     <div>
- <Footer />
+      <Header />
+      <Footer />
     </div>
   );
 };
