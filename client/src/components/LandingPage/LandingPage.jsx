b1960b438733a16dd8040af50711826fe397c9fe
unknown <hruales1138@gmail.com>
2023-01-20 16:11:25 -0500

    galeria

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -3,11 +3,14 @@ import style from "./LandingPage.module.css";
 import Footer from "../Footer/Footer.jsx";
 import Header from "../Header/Header";
 import Services from "../Services/Services.jsx";
+import Galery from "../Galery/Galery";
+
 const LandingPage = () => {
   return (
     <div>
       <Header /> 
       <Services />
+      <Galery />
       <Footer />
     </div>
   );
