81365e12c33ea24fc7a064d6b820f5312ba21184
IgnacioPerezEtc <nachoperezetc@gmail.com>
2023-01-22 17:13:43 -0300

    actualizando diseño

--- client/src/components/SearchBarHotels/SearchBarHotels.jsx
+++ client/src/components/SearchBarHotels/SearchBarHotels.jsx
@@ -15,21 +15,31 @@ const SearchBar = (props) => {
             <span className={style.iconIn}></span>
           </div>
           <div>
-            <select type="text" placeholder="Check-out" className={style.date}>
-              <option value=""></option>
+            <select type="text" placeholder="Languages" className={style.date}>
+              <option value="">Select a language</option>
+              <option value="">Spanish</option>
+              <option value="">Rusian</option>
+              <option value="">English</option>
+              <option value="">French</option>
             </select>
             <span className={style.iconOut}></span>
           </div>
-          <div className={style.containerInput}>
-            <input
-              type="number"
-              placeholder="Adults"
-              className={style.pl}
-              min="0"
-            />
-            <span className={style.iconAdults}></span>
-          </div>
-          <div className={style.containerInput}>
+
+          <select
+            className={style.containerInput}
+            type="number"
+            placeholder="Adults"
+            min="0"
+          >
+            <option value="">Number of stars</option>
+            <option value="">1</option>
+            <option value="">2</option>
+            <option value="">3</option>
+            <option value="">4</option>
+            <option value="">5</option>
+          </select>
+
+          {/* <div className={style.containerInput}>
             <input
               type="number"
               placeholder="Children"
@@ -38,7 +48,7 @@ const SearchBar = (props) => {
             />
 
             <span className={style.iconChildren}></span>
-          </div>
+          </div> */}
 
           <button className={style.buttonSearchBar}>Booking Now</button>
         </div>
