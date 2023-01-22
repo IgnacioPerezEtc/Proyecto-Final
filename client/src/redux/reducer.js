c1934b72c654be1bdb7ab728984f517fb01bab3f
IgnacioPerezEtc <nachoperezetc@gmail.com>
2023-01-22 16:29:01 -0300

    Agregando actions y reducer

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,5 +1,42 @@
-const initialState = {};
+import {
+  CREATE_HOTEL,
+  GET_ALL_HOTELS,
+  GET_HOTEL_BY_ID,
+  GET_HOTEL_BY_NAME,
+} from "./actions";
 
-const rootReducer = (state = initialState, action) => {};
+const initialState = {
+  hotels: [],
+  hotelDetail: {},
+  error: false,
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ALL_HOTELS: {
+      return {
+        ...state,
+        hotels: action.payload,
+      };
+    }
+    case GET_HOTEL_BY_ID: {
+      return {
+        ...state,
+        hotelDetail: action.payload,
+      };
+    }
+    case GET_HOTEL_BY_NAME: {
+      return {
+        ...state,
+        hotels: action.payload,
+      };
+    }
+    case CREATE_HOTEL:
+      return { ...state };
+
+    default:
+      return { ...state };
+  }
+};
 
 export default rootReducer;
