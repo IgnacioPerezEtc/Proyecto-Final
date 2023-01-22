51baedf872e38ae334e39386fd665bed6ebe11c2
Gabriel Bustos <gabrielbustos0905@gmail.com>
2023-01-22 20:32:07 -0300

    feature: Rooms completadas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,9 +8,11 @@ import Hotels from "./components/Hotels/Hotels.jsx";
 import { Provider } from "react-redux";
 
 
-
 import store from "./redux/store.js";
 import AboutUs from "./components/AboutUs/AboutUs.jsx";
+import Detail from "./components/Detail/Detail.jsx";
+import RoomDetail from "./components/RoomDetail/RoomDetail.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <Provider store={store}>
