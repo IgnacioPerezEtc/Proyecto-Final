e468190934d6f84e6895bf54b5fd745fdc1d3eb8
IgnacioPerezEtc <nachoperezetc@gmail.com>
2023-01-26 15:41:37 -0300

    ajustando cambios en la searchbar

--- client/src/components/SearchBarHotels/SearchBarHotels.jsx
+++ client/src/components/SearchBarHotels/SearchBarHotels.jsx
@@ -7,7 +7,7 @@ import {
   getHotelByName,
 } from "../../redux/actions";
 import { useDispatch, useSelector } from "react-redux";
-import Error from "../Error/Error.jsx";
+import { NavLink } from "react-router-dom";
 const SearchBar = (props) => {
   const dispatch = useDispatch();
   const [name, setName] = useState("");
@@ -32,72 +32,66 @@ const SearchBar = (props) => {
     dispatch(getHotelByName(name));
   };
   return (
-    <div className={style.flexContainer}>
-      <div className={style.searchBar}>
-        <div className={style.containerSearchbar}>
-          <div>
-            <input
-              type="text"
-              placeholder="Write a name"
-              className={style.dateSearchbar}
-              onChange={(e) => handleInputChange(e)}
-            />
-            <span className={style.iconIn}></span>
-          </div>
-          <div>
-            <select
-              type="text"
-              placeholder="Languages"
-              className={style.dateSearchbar}
-              onChange={(e) => {
-                handleFilterByLanguage(e);
-              }}
+    <div>
+      <div className={style.flexContainer}>
+        <div className={style.searchBar}>
+          <div className={style.containerSearchbar}>
+            <div>
+              <input
+                type="text"
+                placeholder="Write a name"
+                className={style.dateSearchbar}
+                onChange={(e) => handleInputChange(e)}
+              />
+              <span className={style.iconIn}></span>
+            </div>
+            <button
+              className={style.buttonSearchBar}
+              type="submit"
+              onClick={(e) => handleSubmit(e)}
             >
-              <option value="All">Select a language</option>
-              <option value="spanish">Spanish</option>
-              <option value="russian">Russian</option>
-              <option value="english">English</option>
-              <option value="french">French</option>
-              <option value="german">German</option>
-            </select>
-            <span className={style.iconOut}></span>
+              Booking Now
+            </button>
           </div>
-
-          <select
-            className={style.optionStar}
-            type="number"
-            min="0"
-            onChange={(e) => {
-              handleFilterByStars(e);
-            }}
-          >
-            <option value="All">Number of stars</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-
-          {/* <div className={style.containerInput}>
-            <input
-              type="number"
-              placeholder="Children"
-              className={style.pl}
-              min="0"
-            />
-
-            <span className={style.iconChildren}></span>
-          </div> */}
-
-          <button
-            className={style.buttonSearchBar}
-            type="submit"
-            onClick={(e) => handleSubmit(e)}
-          >
-            Booking Now
-          </button>
         </div>
+      </div>
+      <div>
+        <span className={style.iconOut}></span>
+      </div>
+      <div className={style.flexContainer}>
+        <select
+          className={style.optionStar}
+          type="number"
+          min="0"
+          onChange={(e) => {
+            handleFilterByStars(e);
+          }}
+        >
+          <option value="All">Number of stars</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+        <select
+          type="text"
+          placeholder="Languages"
+          className={style.optionStar}
+          onChange={(e) => {
+            handleFilterByLanguage(e);
+          }}
+        >
+          <option value="All">Select a language</option>
+          <option value="spanish">Spanish</option>
+          <option value="russian">Russian</option>
+          <option value="english">English</option>
+          <option value="french">French</option>
+          <option value="german">German</option>
+        </select>
+            <NavLink to={"/formHotels"}>
+              <button className={style.createHotel}> Create Hotel</button>
+            </NavLink>
       </div>
     </div>
   );
