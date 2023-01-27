ddb9671f27cb8a5e59b7820588ac80bdc26c44c3
SCarmona02 <santiago.car2003@hotmail.com>
2023-01-27 15:19:16 -0500

    cambiosBack

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-conn.sync({ force: true}).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
