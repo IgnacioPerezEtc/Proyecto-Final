3b82333119e3c5c297b7bdae730a8035c8b9eadf
Gabriel Bustos <gabrielbustos0905@gmail.com>
2023-02-02 04:25:52 -0300

    Feature: gran avanze

--- client/src/components/FormHotels/FormHotels.jsx
+++ client/src/components/FormHotels/FormHotels.jsx
@@ -13,7 +13,10 @@ const FormHotels = () => {
   const [hover, setHover] = useState(null);
 
   // --------------- Imagenes Extras -------------------
-  const [imgExt, setImgExt] = useState("");
+  const [imgExt, setImgExt] = useState([]);
+
+  // --------------- Validate ---------------------
+  const [focused, setFocused] = useState(false)
 
   // -------- Estado local de lon Inputs ----------------
   const [input, setInput] = useState({
@@ -99,6 +102,13 @@ const FormHotels = () => {
     })
   };
 
+  const handleFocus = (event) => {
+    setFocused({
+      ...focused,
+      [event.target.name]: true,
+    });
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(input);
@@ -121,62 +131,135 @@ const FormHotels = () => {
   return (
     <div>
       <Header />
-      <h1>Form Hotels</h1>
+      <div className={style.container}>
+        
+          <form className={style.form} onSubmit={(e) => handleSubmit(e)}>
+            <h1>Form Hotels</h1>
 
-      <div>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <div>
-            <label>Name: </label>
-            <input 
-              type="text"
-              name="name"
-              value={input.name}
-              onChange={ (e) => handleChange(e) }
-            />
-            <span></span>
+            <div className={style.containerInputs}>
+              <div>
+                <div className={style.formInput}>
+                  <label>Name: </label>
+                  <input 
+                    type="text"
+                    name="name"
+                    value={input.name}
+                    onChange={ (e) => handleChange(e) }
+                    required
+                    onBlur={(e) => handleFocus(e)}
+                    focused={focused.name?.toString()}
+                  />
+                  <span className={style.span}>Enter a valid Name</span>
+                </div>
+
+                <div className={style.formInput}>
+                  <label>Rooms: </label>
+                  <input 
+                    type="number" 
+                    name="rooms"
+                    value={input.rooms}
+                    onChange={ (e) => handleChange(e) }
+                    required
+                    onBlur={(e) => handleFocus(e)}
+                    focused={focused.rooms?.toString()}
+                  />
+                  <span className={style.span}>Ingrese cantidad de Habitaciones</span>
+                </div>
+              </div>
+
+              <div>
+                <div className={style.formInput}>
+                  <label>Location: </label>
+                  <input 
+                    type="text" 
+                    name="location"
+                    value={input.location}
+                    onChange={ (e) => handleChange(e) }
+                    required
+                    onBlur={(e) => handleFocus(e)}
+                    focused={focused.location?.toString()}
+                  />
+                  <span className={style.span}>Location is required</span>
+                </div>
+
+                <div className={style.formInput}>
+                  <label>Phone: </label>
+                  <input 
+                    type="number" 
+                    name="phone"
+                    value={input.phone}
+                    onChange={ (e) => handleChange(e) }
+                    required
+                    onBlur={(e) => handleFocus(e)}
+                    focused={focused.phone?.toString()}
+                  />
+                  <span className={style.span}>Phone is required</span>
+                </div>
+              </div>
+            </div>
+
+          <div className={style.containerDescription}>
+            <div className={style.formInput}>
+              <label>Description: </label>
+              <textarea 
+                type="text" 
+                name="description"
+                value={input.description}
+                onChange={ (e) => handleChange(e) }
+                required
+                onBlur={(e) => handleFocus(e)}
+                focused={focused.description?.toString()}
+              />
+              <span className={style.span}>Description is required</span>
+            </div>
           </div>
 
-          <div>
-            <label>Rooms: </label>
-            <input 
-              type="number" 
-              name="rooms"
-              value={input.rooms}
-              onChange={ (e) => handleChange(e) }
-            />
+          <div className={style.containerCheck}>
+            <div>
+              
+                <label>Parking: </label>
+                <input 
+                  type="checkbox" 
+                  name="parking"
+                  value={input.parking}
+                  onClick= { (e) => handleCheckbox(e) }
+                  id="switch"
+                  className={style.switch}
+                />
+                <label htmlFor="switch" className={style.lbl}></label>
+              
+            </div>
+
+            <div>
+              <label>Category: </label>
+              {
+                [...Array(5)].map((star, index) => {
+                  const categoryValue = index + 1
+
+                  return (
+                    <label key={index}>
+                        <input 
+                          type="radio" 
+                          name="category" 
+                          value={categoryValue}
+                          onClick= { () => setCategory(categoryValue) }
+                          onChange= { (e) => handleChangeCategory(e)}
+                          className={style.inputRadio}
+                        />
+                        <FaStar
+                          className={style.star}
+                          onMouseEnter={ () => setHover(categoryValue) }
+                          onMouseLeave={ () => setHover(null) }
+                          color={ categoryValue <= (hover || category) ? "#ffc107" : "gray"}
+                        />
+                    </label>
+                  )
+                })
+              }
+            </div>
           </div>
 
-          <div>
-            <label>Location: </label>
-            <input 
-              type="text" 
-              name="location"
-              value={input.location}
-              onChange={ (e) => handleChange(e) }
-            />
-          </div>
-
-          <div>
-            <label>Description: </label>
-            <input 
-              type="text" 
-              name="description"
-              value={input.description}
-              onChange={ (e) => handleChange(e) }
-            />
-          </div>
-
-          <div>
-            <label>Parking: </label>
-            <input 
-              type="checkbox" 
-              name="parking"
-              value={input.parking}
-              onClick= { (e) => handleCheckbox(e) }
-            />
-          </div>
-
-          <div>
+          <div className={style.containerPictureHome}>
             <label>Main picture: </label>
             <input 
               type="text"
@@ -184,113 +267,75 @@ const FormHotels = () => {
               value={input.pictureHome}
               onChange={ (e) => handleChange(e) }
             />
-            <span></span>
+            <span>Enter a URL image .png, .jpg, .jpeg, .gif</span>
           </div>
 
-          <div>
-            <label>Extra Pictures: </label>
-            <input 
-              type="text" 
-              value={imgExt}
-              onChange={ (e) => handleImgExt(e) }
-            />
-            <button onClick={ (e) => handlePlus(e) } name="imgExt">+</button>
-            {
-              input.pictureDetail?.map((img, index)=> {
-                  return(
-                    <div key={index}>
-                      <span key={img}>{img}</span>
-                      <button onClick={(event) => handleDeleteImg(event)} name={img}>X</button>
-                    </div>
-                  )
-              })
-            }
-          </div>
 
-          <div>
-            <label>Rating: </label>
-            <input 
-              type="number" 
-              name="rating"
-              value={input.rating}
-              onChange= { (e) => handleChange(e) }
-            />
-          </div>
-
-          <div>
-            <label>Languages: </label>
-            <select name="languages" onChange={ (e) => handleSelect(e) }>
-              {
-                languages.map((languages, index) => {
-                  return (
-                    <option key={index}>{languages}</option>
-                  )
-                })
-              }
-            </select>
             <div>
+              <label>Extra Pictures: </label>
+              <input 
+                type="text" 
+                value={imgExt}
+                onChange={ (e) => handleImgExt(e) }
+              />
+              <button onClick={ (e) => handlePlus(e) } name="imgExt">+</button>
               {
-                input.languages?.map((languages) => {
-                  return (
-                    <div key={languages}>
-                      <p key={languages}>{languages}</p>
-                      <button
-                        value={languages}
-                        onClick={(event) => handleDelete(event)}
-                      >
-                        x
-                      </button>
-                    </div>
-                  );
+                input.pictureDetail?.map((img, index)=> {
+                    return(
+                      <div key={index}>
+                        <span key={img}>{img}</span>
+                        <button onClick={(event) => handleDeleteImg(event)} name={img}>X</button>
+                      </div>
+                    )
                 })
               }
             </div>
-          </div>
 
-          <div>
-            
-            {
-              [...Array(5)].map((star, index) => {
-                const categoryValue = index + 1
+            <div>
+              <label>Rating: </label>
+              <input 
+                type="number" 
+                name="rating"
+                value={input.rating}
+                onChange= { (e) => handleChange(e) }
+              />
+            </div>
 
-                return (
-                  <label key={index}>
-                      <input 
-                        type="radio" 
-                        name="category" 
-                        value={categoryValue}
-                        onClick= { () => setCategory(categoryValue) }
-                        onChange= { (e) => handleChangeCategory(e)}
-                        className={style.inputRadio}
-                      />
-                      <FaStar 
-                        className={style.star}
-                        onMouseEnter={ () => setHover(categoryValue) }
-                        onMouseLeave={ () => setHover(null) }
-                        color={ categoryValue <= (hover || category) ? "#ffc107" : "white"}
-                      />
-                  </label>
-                )
-              })
-            }
-          </div>
+            <div>
+              <label>Languages: </label>
+              <select name="languages" onChange={ (e) => handleSelect(e) }>
+                {
+                  languages.map((languages, index) => {
+                    return (
+                      <option key={index}>{languages}</option>
+                    )
+                  })
+                }
+              </select>
+              <div>
+                {
+                  input.languages?.map((languages) => {
+                    return (
+                      <div key={languages}>
+                        <p key={languages}>{languages}</p>
+                        <button
+                          value={languages}
+                          onClick={(event) => handleDelete(event)}
+                        >
+                          x
+                        </button>
+                      </div>
+                    );
+                  })
+                }
+              </div>
+            </div>
 
-          <div>
-            <label>Phone: </label>
-            <input 
-              type="number" 
-              name="phone"
-              value={input.phone}
-              onChange={ (e) => handleChange(e) }
-            />
-          </div>
-
-          <div>
-            <button type="submit">Create Hotel</button>
-          </div>
-        </form>
-      </div>
-
+            <div>
+              <button className={style.buttonSubmit} type="submit">Create Hotel</button>
+            </div>
+          </form>
+        </div>
       <Footer />
     </div>
   )
