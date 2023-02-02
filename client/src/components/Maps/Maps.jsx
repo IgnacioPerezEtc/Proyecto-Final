6356a5952032d3546ec9a627e06d213b992c9898
IgnacioPerezEtc <nachoperezetc@gmail.com>
2023-02-02 20:35:46 -0300

    agregando maps con localizacion

--- client/src/components/Maps/Maps.jsx
+++ client/src/components/Maps/Maps.jsx
@@ -1,18 +1,48 @@
 import React from "react";
-import { MapContainer, Marker, TileLayer,Popup } from "react-leaflet";
+import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
 import style from "./Maps.module.css";
-import 'leaflet/dist/leaflet.css';
+import "leaflet/dist/leaflet.css";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getAllHotels } from "../../redux/actions";
+import { useDispatch } from "react-redux";
+import Hotels from "../Hotels/Hotels";
 const Maps = () => {
-  return (
-    <MapContainer className={style.map} center={[51.505, -0.09]} zoom={13}>
-    <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>51.505, -0.09</Popup>
-  </Marker>
-  </MapContainer>
-  );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllHotels());
+  }, [dispatch]);
+  const hotels = useSelector((state) => state.hotels);
+  if (hotels.length) {
+    return (
+      <MapContainer
+        className={style.map}
+        center={[3.4368376956417364, -76.52370312288105]}
+        zoom={13}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {hotels.map((hotel) => {
+          let array = [];
+          hotel.position.map((hotelPosition) => {
+            array.push(parseFloat(hotelPosition));
+          });
+          return (
+            <Marker key={array} position={array}>
+              <Popup>51.505, -0.09</Popup>
+            </Marker>
+          );
+        })}
+      </MapContainer>
+    );
+  } else {
+    return (
+      <div>
+        <p>loading</p>
+      </div>
+    );
+  }
 };
 export default Maps;
