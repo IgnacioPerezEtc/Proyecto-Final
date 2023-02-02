b4b5961772acf1a60af0a4035a674b1463511af6
unknown <hruales1138@gmail.com>
2023-02-01 23:17:33 -0500

    payApproved

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import axios from 'axios'
+// axios.defaults.baseURL = "http://localhost:3001";
 axios.defaults.baseURL = "https://proyecto-final-production-e80c.up.railway.app/";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
