808529f47badd86273e6e2af557a218b705df790
Gabriel Bustos <gabrielbustos0905@gmail.com>
2023-02-04 02:33:48 -0300

    Feature: Form-Rooms

--- client/src/components/FormRooms/FormRooms.jsx
+++ client/src/components/FormRooms/FormRooms.jsx
@@ -107,160 +107,170 @@ const FormRooms = () => {
   return (
     <div>
       <Header />
+      <div className={style.firsContainer}>
 
-      <div className={style.container}>
-        <p className="fw-bold text-center display-1">
-           Create <span className="text-danger">Room</span>
-        </p>
+        <div className={style.container}>
+          <p className="fw-bold text-center display-1">
+            Create <span className="text-danger">Room</span>
+          </p>
 
-        <form onSubmit={(e) => handleSubmit(e)} className={style.form}>
+          <form onSubmit={(e) => handleSubmit(e)} className={style.form}>
 
-          <div className={style.containerInputs}>
-            <div>
-              <div className={style.containerInput}>
-                <label>Name Room: </label>
+            <div className={style.containerInputs}>
+              <div>
+                <div className={style.containerInput}>
+                  <label>Name Room: </label>
+                  <input 
+                    type="text" 
+                    name="numRoom"
+                    placeholder="Name Room"
+                    value={input.name}
+                    onChange= {(e) => handleChange(e)}
+                  />
+                  <div>
+                    <span className={style.span}>{inputErrors.numRoom}</span> 
+                  </div>
+                </div>
+
+                <div className={style.containerInput}>
+                  <label>Max of Adult: </label>
+                  <input 
+                    type="number" 
+                    name="maxAdult"
+                    value={input.maxAdult}
+                    onChange= {(e) => handleChange(e)}
+                  />
+                  <div>
+                    <span className={style.span}>{inputErrors.maxAdult}</span>
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <div className={style.containerInput}>
+                  <label>Number of Peoples: </label>
+                  <input 
+                    type="number" 
+                    name="numPeople"
+                    value={input.numPeople}
+                    onChange= {(e) => handleChange(e)}
+                  />
+                  <div>
+                    <span className={style.span}>{inputErrors.numPeople}</span>
+                  </div>
+                </div>
+
+                <div className={style.containerInput}>
+                  <label>Max of Child: </label>
+                  <input 
+                    type="number" 
+                    name="maxChild"
+                    value={input.maxChild}
+                    onChange= {(e) => handleChange(e)}
+                  />
+                  <div>
+                    <span className={style.span}>{inputErrors.maxChild}</span>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+            
+            <div className={style.containerDescription}>
+              <label>Description: </label>
+              <textarea 
+                type="text"
+                name="description"
+                value={input.description}
+                onChange= {(e) => handleChange(e)}
+              />
+              <div>
+                <span className={style.span}>{inputErrors.description}</span>
+              </div>
+            </div>
+
+            <div className={style.containerPictureHome}>
+                <label>Picture Home: </label>
                 <input 
                   type="text" 
-                  name="numRoom"
-                  placeholder="Name Room"
-                  value={input.name}
+                  name="pictureHome"
+                  value={input.pictureHome}
                   onChange= {(e) => handleChange(e)}
                 />
                 <div>
-                  <span className={style.span}>{inputErrors.numRoom}</span> 
+                  <span className={style.span}>{inputErrors.pictureHome}</span>
                 </div>
-              </div>
-
-              <div className={style.containerInput}>
-                <label>Max of Adult: </label>
-                <input 
-                  type="number" 
-                  name="maxAdult"
-                  value={input.maxAdult}
-                  onChange= {(e) => handleChange(e)}
-                />
-                <div>
-                  <span className={style.span}>{inputErrors.maxAdult}</span>
-                </div>
-              </div>
             </div>
 
-            <div>
-              <div className={style.containerInput}>
-                <label>Number of Peoples: </label>
-                <input 
-                  type="number" 
-                  name="numPeople"
-                  value={input.numPeople}
-                  onChange= {(e) => handleChange(e)}
-                />
-                <div>
-                  <span className={style.span}>{inputErrors.numPeople}</span>
-                </div>
-              </div>
-
-              <div className={style.containerInput}>
-                <label>Max of Child: </label>
-                <input 
-                  type="number" 
-                  name="maxChild"
-                  value={input.maxChild}
-                  onChange= {(e) => handleChange(e)}
-                />
-                <div>
-                  <span className={style.span}>{inputErrors.maxChild}</span>
-                </div>
-              </div>
-            </div>
-
-          </div>
-          
-          <div className={style.containerDescription}>
-            <label>Description: </label>
-            <textarea 
-              type="text"
-              name="description"
-              value={input.description}
-              onChange= {(e) => handleChange(e)}
-            />
-            <div>
-              <span className={style.span}>{inputErrors.description}</span>
-            </div>
-          </div>
-
-          <div className={style.containerPictureHome}>
-              <label>Picture Home: </label>
+            <div className={style.containerPictureHome}>
+              <label>Picture Detail: </label>
               <input 
                 type="text" 
-                name="pictureHome"
-                value={input.pictureHome}
-                onChange= {(e) => handleChange(e)}
               />
-              <div>
-                <span className={style.span}>{inputErrors.pictureHome}</span>
-              </div>
-          </div>
-
-          <div className={style.containerPictureHome}>
-            <label>Picture Detail: </label>
-            <input 
-              type="text" 
-            />
-          </div>
-
-          <div className={style.containerService}>
-            <label>Specialties: </label>
-            <div className={style.containerSpecialties}>
-              {
-                specialties.map((specialties, index) => {
-                  return (
-                    <div key={index}>
-                      <label>{specialties}</label>
-                      <input 
-                        type="checkbox" 
-                        name="specialties"
-                        value={specialties}
-                        id={`switch${index}`}
-                        className={style.switch}
-                        onChange={(e) => handleChecked(e)}
-                      />
-                      <label htmlFor={`switch${index}`} className={style.lbl}></label>
-                    </div>
-                  )
-                })
-              }
             </div>
-          </div>
 
-          <div className={style.containerValue}>
-            <label>Value: </label>
+            <div className={style.containerService}>
+              <label>Specialties: </label>
+              <div className={style.containerSpecialties}>
+                {
+                  specialties.map((specialties, index) => {
+                    return (
+                      <div key={index}>
+                        <label>{specialties}</label>
+                        <input 
+                          type="checkbox" 
+                          name="specialties"
+                          value={specialties}
+                          id={`switch${index}`}
+                          className={style.switch}
+                          onChange={(e) => handleChecked(e)}
+                        />
+                        <label htmlFor={`switch${index}`} className={style.lbl}></label>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            </div>
+
+            <div className={style.containerValue}>
+              <label>Value: </label>
+              <div>
+                <input 
+                  type="number" 
+                  name="value"
+                  value={input.value}
+                  onChange= {(e) => handleChange(e)}
+                />
+                <div>
+                  <span className={style.span}>{inputErrors.value}</span>
+                </div>
+              </div>
+            </div>
+
             <div>
+              <label>Hidden: </label>
               <input 
-                type="number" 
-                name="value"
-                value={input.value}
-                onChange= {(e) => handleChange(e)}
+                type="checkbox" 
+                name="hidden"
+                value={input.hidden}
+                onChange={(e) => handleCheckedHidden(e)}
               />
-              <div>
-                <span className={style.span}>{inputErrors.value}</span>
+            </div>
+
+            <div>
+                <button className={style.buttonSubmit} type="submit">Create Room</button>
               </div>
-            </div>
-          </div>
+          </form>
+        </div>
 
+        <div className={style.containerImages}>
+          <h2>Picture Home: </h2>
+          <img src={input.pictureHome ? input.pictureHome : "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png"}/>
           <div>
-            <label>Hidden: </label>
-            <input 
-              type="checkbox" 
-              name="hidden"
-              value={input.hidden}
-              onChange={(e) => handleCheckedHidden(e)}
-            />
-          </div>
 
-          <div>
-              <button className={style.buttonSubmit} type="submit">Create Room</button>
-            </div>
-        </form>
+          </div>
+        </div>
       </div>
 
       <Footer />
