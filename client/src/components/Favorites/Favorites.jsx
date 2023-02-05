73e8a032d3c41e5f0c85d214063b527b2f91a290
unknown <hruales1138@gmail.com>
2023-02-05 09:48:04 -0500

    favorites

--- client/src/components/Favorites/Favorites.jsx
+++ client/src/components/Favorites/Favorites.jsx
@@ -3,14 +3,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAllHotels, getFavoriteHotels } from "../../redux/actions";
 import HotelCard from "../HotelCard/HotelCard";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Keyboard } from "swiper";
+import { Keyboard, Autoplay } from "swiper";
 import "swiper/css";
 import "swiper/css/free-mode";
-import style from './Favorites.module.css'
+import style from './Favorites.module.css';
 
 
 const Favorites = () => {
-  
+
   const dispatch = useDispatch();
   const user = (JSON.parse(localStorage.getItem("user")));
   const allHotels = useSelector(state => state.allHotels);
@@ -19,71 +19,77 @@ const Favorites = () => {
 
   useEffect(() => {
     dispatch(getAllHotels())
-    dispatch( getFavoriteHotels(user[0].email) )
+    dispatch(getFavoriteHotels(user[0].email))
     console.log(favs.length)
   }, [dispatch, favs.length])
 
 
   return (
-
     <div>
-      <div className={style.titleContainer}>
-        <h2 className={style.blue}>Favorite</h2>
-        <h2 className={style.red}>Hotels</h2>
-      </div>
-      <div className="w-100 d-flex gap-5 flex-wrap justify-content-center">
-        <Swiper
-          freeMode={true}
-          grabCursor={true}
-          modules={[Keyboard]}
+      {hotels.length ? (
+        <div>
+          <div className={style.titleContainer}>
+            <h2 className={style.blue}>Favorite</h2>
+            <h2 className={style.red}>Hotels</h2>
+          </div>
 
-          keyboard={{
-            enabled: true,
-          }}
-          className="mySwiper m-4 justify-content-center w-100"
-          breakpoints={{
-            0: {
-              slidesPerView: 1,
-              spaceBetween: 30,
-              centeredSlides: true,
-            },
-            480: {
-              slidesPerView: 1,
-              spaceBetween: 15,
-              centeredSlides: true,
-            },
-            768: {
-              slidesPerView: 2,
-              spaceBetween: 10,
-            },
-            1024: {
-              slidesPerView: 3,
-              spaceBetween: 30,
-            },
-            1440: {
-              slidesPerView: 4,
-              spaceBetween: 20,
-            },
-          }}
-        >
-          {
-            hotels.map((hotel) => {
-              return (
-                <SwiperSlide key={hotel.id}>
-                  <HotelCard
-                    id={hotel.id}
-                    name={hotel.name}
-                    image={hotel.pictureHome}
-                    category={hotel.category}
-                    languages={hotel.languages}
-                  />
-                </SwiperSlide>
-              );
-            })
-          }
-        </Swiper>
+          <div className="m-5">
+            <Swiper
+              freeMode={true}
+              grabCursor={true}
+              modules={[Autoplay, Keyboard]}
+              autoplay={{
+                delay: 3000,
+              }}
+              keyboard={{
+                enabled: true,
+              }}
+              className="mySwiper m-4 justify-content-center w-100"
+              breakpoints={{
+                0: {
+                  slidesPerView: 1,
+                  spaceBetween: 30,
+                  centeredSlides: true,
+                },
+                480: {
+                  slidesPerView: 1,
+                  spaceBetween: 15,
+                  centeredSlides: true,
+                },
+                768: {
+                  slidesPerView: 2,
+                  spaceBetween: 10,
+                },
+                1024: {
+                  slidesPerView: 3,
+                  spaceBetween: 30,
+                },
+                1440: {
+                  slidesPerView: 4,
+                  spaceBetween: 20,
+                },
+              }}
+            >
+              {
+                hotels.map((hotel) => {
+                  return (
+                    <SwiperSlide key={hotel.id}>
+                      <HotelCard
+                        id={hotel.id}
+                        name={hotel.name}
+                        image={hotel.pictureHome}
+                        category={hotel.category}
+                        languages={hotel.languages}
+                      />
+                    </SwiperSlide>
+                  );
+                })
+              }
+            </Swiper>
+          </div>
+        </div>
+      ) : (<h1 className={style.loading}>Loading...</h1>)}
 
-      </div>
     </div>
 
   )
