e18a1803d789b99208b9a187526d561c9f3fdee9
unknown <hruales1138@gmail.com>
2023-02-08 14:26:18 -0500

    editar_usuario

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -233,6 +233,13 @@ export function getUserById(id) {
   };
 }
 
+export const putUser = (id, data) => {
+  return async () => {
+    const json = await axios.put(`/user/edit/${id}`, data);
+    return json;
+  };
+};
+
 // *********************** SEND EMAIL ************************
 
 export const sendEmail = async (data, form) => {
