d04d2289d1d6f5120b58d4a31386025cc633c6c5
unknown <hruales1138@gmail.com>
2023-02-08 11:03:59 -0500

    editar_usuario

--- client/src/components/UserEdit/UserEdit.jsx
+++ client/src/components/UserEdit/UserEdit.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import style from './UserEdit.module.css';
 
 const UserEdit = () => {
     return(
-        <div>
-            UserEdit
+        <div className={style.mainContainer}>
+            <form id={style.formContainer}>
+                <h1>Edit your information</h1> 
+               <label className={style.form}>
+                    <h4>User Name:</h4>            
+                </label> 
+                <input className={style.form}/>
+
+                <label className={style.form}>
+                    <h4>Address:</h4>            
+                </label> 
+                <input className={style.form}/>
+
+                <label className={style.form}>
+                    <h4>Phone Number:</h4>            
+                </label> 
+                <input className={style.form}/>
+
+                <button id={style.button} type="submit">Save Changes</button>
+            </form>
         </div>
     )
 };
